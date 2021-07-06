295c2692cbb816657c17545927c7b9764a46d986
Aditya1028 <adityaa750@gmail.com>
2021-07-06 19:06:21 +0530

    wildfly changes

--- spring-boot-h2-database-example/src/main/java/com/atradius/cibt/SpringBootH2DatabaseExampleApplication.java
+++ spring-boot-h2-database-example/src/main/java/com/atradius/cibt/SpringBootH2DatabaseExampleApplication.java
@@ -2,10 +2,18 @@ package com.atradius.cibt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootH2DatabaseExampleApplication {
+public class SpringBootH2DatabaseExampleApplication extends SpringBootServletInitializer{
+	
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+	    return builder.sources(SpringBootH2DatabaseExampleApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootH2DatabaseExampleApplication.class, args);
 	}
 }
+	
