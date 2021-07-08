930c726b0ca036f3130733e0fb93a3d8a5f746b9
Aditya1028 <adityaa750@gmail.com>
2021-07-08 12:23:26 +0530

    Adding logs

--- spring-boot-h2-database-example/src/main/java/com/atradius/cibt/controller/FoodManagmentControllerImpl.java
+++ spring-boot-h2-database-example/src/main/java/com/atradius/cibt/controller/FoodManagmentControllerImpl.java
@@ -3,6 +3,7 @@ package com.atradius.cibt.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -10,21 +11,28 @@ import org.springframework.stereotype.Component;
 import com.atradius.cibt.model.Food;
 import com.atradius.cibt.model.Response;
 import com.atradius.cibt.service.FoodManagementService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Component
 public class FoodManagmentControllerImpl implements FoodManagmentController {
 
 	@Autowired
 	FoodManagementService foodManagementService;
+	
+	private static final Logger LOGGER = LogManager.getLogger(FoodManagmentControllerImpl.class); 
 
-	@Override
+	@Override	
+	@Cacheable(value = "food", key = "#id")
 	public ResponseEntity getFoods(String contentType, String authorization) {
+		LOGGER.info("Inside getFoods controller");
 		List<Food> foods = foodManagementService.getFoods();
 		return new ResponseEntity(foods, HttpStatus.OK);
 	}
 
 	@Override
 	public ResponseEntity addFood(String contentType, String authorization, Food food) {
+		LOGGER.info("Inside addFood controller");
 		foodManagementService.addFood(food);
 		return new ResponseEntity(food.getId(), HttpStatus.CREATED);
 	}
@@ -32,6 +40,7 @@ public class FoodManagmentControllerImpl implements FoodManagmentController {
 	@SuppressWarnings("unchecked")
 	@Override
 	public ResponseEntity removeFood(String contentType, String authorization, Long foodId) {
+		LOGGER.info("Inside removeFood controller");
 		Boolean result = foodManagementService.removeFood(foodId);
 		if (result) {
 			return new ResponseEntity(result, (result) ? HttpStatus.NO_CONTENT : HttpStatus.NOT_FOUND);
--- spring-boot-h2-database-example/src/main/java/com/atradius/cibt/daoImpl/FoodDaoImpl.java
+++ spring-boot-h2-database-example/src/main/java/com/atradius/cibt/daoImpl/FoodDaoImpl.java
@@ -2,8 +2,9 @@ package com.atradius.cibt.daoImpl;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import com.atradius.cibt.dao.FoodDao;
@@ -16,21 +17,24 @@ public class FoodDaoImpl implements FoodDao {
 	@Autowired
 	private FoodRepository foodRepository;
 
+	private static final Logger LOGGER = LogManager.getLogger(FoodDaoImpl.class);	
 	
 	@Override
-	@Cacheable(value = "food", key = "#id")
 	public List<FoodEntity> getFoods() {
-		return foodRepository.findAll();
+		LOGGER.info("Inside getFoods Dao");
+		return foodRepository.findAll();	
 	}
 
 	@Override
 	public Boolean addFood(FoodEntity food) {
+		LOGGER.info("Inside addFood Dao");
 		foodRepository.save(food);
 		return true;
 	}
 
 	@Override
 	public Boolean removeFood(Long foodId) {
+		LOGGER.info("Inside removeFood Dao");
 		if (foodRepository.existsById(foodId)) {
 			foodRepository.deleteById(foodId);
 			return true;
